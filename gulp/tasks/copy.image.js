ead42be33679e51bbfd7174f972da6cd158d5d6d
Alexandr <vorblex@gmail.com>
2017-04-23 00:13:21 +0300

    Complete about page

--- gulp/tasks/copy.image.js
+++ gulp/tasks/copy.image.js
@@ -2,7 +2,7 @@
 
 module.exports = function() {
     $.gulp.task('copy:image', function() {
-        return $.gulp.src('./source/images/**/*.{png,jpg,svg}', { since: $.gulp.lastRun('copy:image') })
+        return $.gulp.src('./source/images/**/*.{png,jpg}', { since: $.gulp.lastRun('copy:image') })
             .pipe($.gp.imagemin())
             .pipe($.gulp.dest($.config.root + '/assets/img'));
     });
--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,27 @@
 (function () {
   'use strict';
-  if (document.querySelector('.greating_picture')) {
-    setTimeout(function () {
-      document
-        .querySelector('.greating_picture')
-        .classList
-        .add('m--show');
-    }, 1000);
-  }
+
+    // ymaps.ready(init);
+    // var myMap;
+    //
+    // function init(){
+    //     myMap = new ymaps.Map("map", {
+    //         center: [50.45457027, 30.52303845],
+    //         zoom: 17,
+    //         controls: ["zoomControl"]
+    //     });
+    //     myMap.behaviors.disable('scrollZoom');
+    // }
+
+
+    mapboxgl.accessToken = 'pk.eyJ1Ijoidm9yYmxleCIsImEiOiJjajF0bWE0dDQwMTM1MndrbTJoYWljY2RyIn0.Nwh76e0A45uCbMtTGVsgxw';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [-74.50, 40], // starting position
+        zoom: 9, // starting zoom
+        scrollZoom: false
+    });
 
 })();
 
@@ -16,13 +30,13 @@
 // module.func1();
 // module.func2();
 
-import {square, MyClass} from './module';
-
-console.log(square(5));
-var cred = {
-  name: 'Юрий Кучма',
-  enrollmentNo: 11115078
-};
-var x = new
-MyClass(cred);
-console.log(x.getName());
+// import {square, MyClass} from './module';
+//
+// console.log(square(5));
+// var cred = {
+//   name: 'Юрий Кучма',
+//   enrollmentNo: 11115078
+// };
+// var x = new
+// MyClass(cred);
+// console.log(x.getName());
