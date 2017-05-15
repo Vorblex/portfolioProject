5274e72a24c31484f4154d4f4ba36dbb64f4daa7
Alexandr <vorblex@gmail.com>
2017-05-15 11:42:32 +0300

    Homework 4 complete

---
+++ source/js/slider.js
@@ -0,0 +1,271 @@
+var sliderFunc = (function () {
+  var Slider = function (container) {
+    var
+      nextBtn     = container.find('.works-slider__control-btn_left'),
+      prevBtn     = container.find('.works-slider__control-btn_right'),
+      items       = nextBtn.find('.works-slider__control-item'),
+      display     = container.find('.works-slider__display'), // Витрина слайдера
+      title       = container.find('.subtitle'),
+      skills      = container.find('.works__content-desc'),
+      link        = container.find('.works__content-view'),
+      itemsLength = items.length,
+      duration    = 500,
+      flag        = true;
+
+    var timeout;
+
+    this.counter = 0;
+
+
+    var generateMarkups = function() {
+      var list = nextBtn.find('.works-slider__control-list'),
+        markups = list.clone();
+
+      prevBtn
+                .append(markups)
+                .find('.works-slider__control-item')
+                .removeClass('active')
+                // .eq(this.counter + 1)
+                .addClass('active');
+    };
+
+    var getDataArrays = function() {
+      var dataObject = {
+        pics : [],
+        title : [],
+        skills : [],
+        link : []
+      };
+
+      $.each(items, function() {
+        var $this = $(this);
+
+        dataObject.pics.push($this.data('full'));
+        dataObject.title.push($this.data('title'));
+        dataObject.skills.push($this.data('skills'));
+        dataObject.link.push($this.data('link'));
+      });
+
+      return dataObject;
+    };
+
+    var slideInLeftBtn = function(slide) {
+      var
+        reqItem = items.eq(slide - 1),
+        activeItem = items.filter('.active');
+
+      activeItem
+                .stop(true, true)
+                .animate({'top' : '100%'}, duration);
+
+      reqItem
+                .stop(true, true)
+                .animate({'top' : '0%'}, duration, function () {
+                  $(this).addClass('active')
+                        .siblings().removeClass('active')
+                        .css('top', '-100%');
+                });
+
+
+    };
+
+    var slideInRightBtn = function (slide) {
+      var
+        items = prevBtn.find('.works-slider__control-item'),
+        activeItem = items.filter('.active'),
+        reqSlide = slide + 1;
+
+      if (reqSlide > itemsLength - 1) {
+        reqSlide = 0;
+      }
+
+      var reqItem = items.eq(reqSlide);
+
+      activeItem
+                .stop(true, true)
+                .animate({'top' : '-100%'}, duration);
+
+      reqItem
+                .stop(true, true)
+                .animate({'top' : '0%'}, duration, function () {
+                  $(this).addClass('active')
+                        .siblings().removeClass('active')
+                        .css('top', '100%');
+                });
+    };
+
+    var changeMainPicture = function(slide) {
+      var image = display.find('.works-slider__display-pic');
+      var data = getDataArrays();
+
+      image
+                .stop(true, true)
+                .fadeOut(duration / 2, function() {
+                  image.attr('src', data.pics[slide]);
+                  $(this).fadeIn(duration / 2);
+                });
+    };
+
+    var changeTextData = function(slide) {
+      var data = getDataArrays();
+
+
+      aviatitle.generate(data.title[slide], title, 'ru');
+
+
+      aviatitle.generate(data.skills[slide], skills, 'en');
+
+
+      link.attr('href', data.link[slide]);
+    };
+
+
+    this.setDefaults = function() {
+      var
+        _that = this,
+        data = getDataArrays();
+
+
+      generateMarkups();
+
+
+      nextBtn
+                .find('.works-slider__control-item')
+                .eq(_that.counter - 1)
+                .addClass('active');
+
+
+      prevBtn
+                .find('.works-slider__control-item')
+                .eq(_that.counter + 1)
+                .addClass('active');
+
+
+      display
+                .find('.works-slider__display-pic')
+                .attr('src', data.pics[_that.counter]);
+
+
+      changeTextData(_that.counter);
+
+    };
+
+    this.moveSlide = function(direction) {
+      var _that = this;
+
+      var directions = {
+        next : function() {
+
+          if (_that.counter < itemsLength - 1) {
+            _that.counter++;
+          } else {
+            _that.counter = 0;
+          }
+        },
+
+        prev : function () {
+          if (_that.counter > 0) {
+            _that.counter--;
+          } else {
+            _that.counter = itemsLength - 1;
+          }
+        }
+      };
+
+      directions[direction]();
+
+      if (flag) {
+        flag = false;
+
+        if (typeof timeout != 'undefined') {
+          clearTimeout(timeout);
+        }
+
+        timeout = setTimeout(function () {
+          flag = true;
+        }, duration + 50);
+
+        slideInLeftBtn(_that.counter);
+        slideInRightBtn(_that.counter);
+        changeMainPicture(_that.counter);
+        changeTextData(_that.counter);
+      }
+    };
+  };
+
+  return {
+    init: function () {
+      var slider = new Slider($('.works'));
+      slider.setDefaults();
+
+      $('.works-slider__control-btn_left').on('click', function(e){
+        e.preventDefault();
+        slider.moveSlide('prev');
+      });
+
+      $('.works-slider__control-btn_right').on('click', function(e){
+        e.preventDefault();
+        slider.moveSlide('next');
+      });
+    }
+  };
+}());
+
+export {sliderFunc};
+
+
+
+
+
+
+var aviatitle = {
+  generate : function (string, block) {
+    var
+      wordsArray = string.split(' '),
+      stringArray = string.split(''),
+      sentence = [],
+      word = '';
+
+    block.text('');
+
+    wordsArray.forEach(function(currentWord) {
+      var wordsArray = currentWord.split('');
+
+      wordsArray.forEach(function(letter) {
+        var letterHtml = '<span class="letter-span">' + letter + '</span>';
+
+        word += letterHtml;
+      });
+
+      var wordHTML = '<span class="letter-word">' + word + '</span>';
+
+      sentence.push(wordHTML);
+      word = '';
+    });
+
+    block.append(sentence.join(' '));
+
+
+    var
+      letters = block.find('.letter-span'),
+      counter = 0,
+      timer,
+      duration = 500 / stringArray.length; 
+
+    function showLetters() {
+      var currentLetter = letters.eq(counter);
+
+      currentLetter.addClass('active');
+      counter++;
+
+      if (typeof timer !== 'undefined') {
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(showLetters, duration);
+    }
+
+    showLetters();
+
+  }
+};
